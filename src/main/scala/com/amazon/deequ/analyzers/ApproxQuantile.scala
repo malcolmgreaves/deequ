493f77275e8890fc0c9074240f589c31cf17b5c5
Malcolm Greaves <malcolm.greaves@changehealthcare.com>
2019-02-12 17:32:40 -0800

    Fixed ApproxQuantile

--- src/main/scala/com/amazon/deequ/analyzers/ApproxQuantile.scala
+++ src/main/scala/com/amazon/deequ/analyzers/ApproxQuantile.scala
@@ -19,10 +19,11 @@ package com.amazon.deequ.analyzers
 import com.amazon.deequ.analyzers.Preconditions.{hasColumn, isNumeric}
 import com.amazon.deequ.analyzers.runners.{IllegalAnalyzerParameterException, MetricCalculationException}
 import com.amazon.deequ.metrics.DoubleMetric
+import com.amazon.deequ.schema.ColumnName
 import org.apache.spark.sql.{DeequFunctions, Row}
 import org.apache.spark.sql.catalyst.expressions.aggregate.ApproximatePercentile
 import org.apache.spark.sql.catalyst.expressions.aggregate.ApproximatePercentile.PercentileDigest
-//import org.apache.spark.sql.functions.col
+import org.apache.spark.sql.functions.col
 import org.apache.spark.sql.types.StructType
 
 case class ApproxQuantileState(percentileDigest: PercentileDigest)
@@ -61,7 +62,7 @@ case class ApproxQuantile(column: String, quantile: Double, relativeError: Doubl
   }
 
   override private[deequ] def aggregationFunctions() = {
-    DeequFunctions.stateful_approx_quantile(col(column), relativeError) :: Nil
+    DeequFunctions.stateful_approx_quantile(col(ColumnName.sanitize(column)), relativeError) :: Nil
   }
 
   override private[deequ] def fromAggregationResult(
